eacb2075d93512e70e3a6d9eb2f22cb0873d5aa3
meta00002 <yangji0099@yahoo.com>
2022-06-14 23:39:22 +0800

    002

--- src/utils/configuration.js
+++ src/utils/configuration.js
@@ -1,17 +1,17 @@
 // Set smart contract based on chain id
 export const CONTRACTADDRESS = {
-    1: "",
+    1: "0x8fA68660D7F240D07e8eDEc24A95FC63ADdb034E",
     4: "",
 };
 
 // Set the price of your NFT
-export const PRICE = 0;
+export const PRICE = 0.005;
 
 // Set the max amount of your NFT collection
-export const MAX_AMOUNT = 1000;
+export const MAX_AMOUNT = 10000;
 
 // Set the max batch size per wallet 
-export const MAX_BATCH_SIZE = 9;
+export const MAX_BATCH_SIZE = 3;
 
 // Set the opesnea url 
-export const OPENSEA_URL = "https://opensea.io/collection/"
+export const OPENSEA_URL = "https://opensea.io/collection/meta-doge-v2-1/"
